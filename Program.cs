2e5e84d02699a5537d0023ee4369a20f9a590d43
Mitdia <knorre.mitya@mail.ru>
2023-09-27 23:32:21 +0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using Bert;
+
+namespace BertCs 
+{
+    internal class Program
+    {
+        static async Task Main(string[] args) 
+        {
+            if (args.Length == 0)
+            {
+                Console.WriteLine("Please provide the name of the txt file as a console argument.");
+                return;
+            }
+            
+            string fileName = args[0];
+            var consoleUI = new ConsoleUI();
+            CancellationTokenSource ctf = new CancellationTokenSource();
+            var model = new BertModel(fileName, consoleUI, ctf.Token);
+            await model.AnswerQuestions(ctf.Token);
+        }
+    }
+
+    public class ConsoleUI : IUIServices
+    {
+        public void ReportError(string message) 
+        {
+            Console.WriteLine(message);
+            Environment.Exit(0);
+        }
+
+        public void Answer(string message)
+        {
+            Console.WriteLine(message);
+        }
+
+        public string?  GetQuestion()
+        {
+            return Console.ReadLine();
+        }
+    }
+}
